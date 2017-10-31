acfd5a745b685f74fa4104d47f6efc2501108584
maxirok <a.czajczyk@hotmail.com>
2017-10-31 18:58:49 +0100

    js for key press

--- app.js
+++ app.js
@@ -2,6 +2,43 @@
     document.addEventListener('DOMContentLoaded', function() {
         
         
+function keyDownSound(e) {
+  var keyCode = e.keyCode;
+    switch (keyCode) {
+        case 65:
+        alert("you hit a key");
+        break;
+        case 83:
+        alert("you hit s key");
+        break;
+        case 68:
+        alert("you hit d key");
+        break;
+        case 70:
+        alert("you hit f key");
+        break;
+        case 71:
+        alert("you hit g key");
+        break;
+        case 72:
+        alert("you hit h key");
+        break;
+        case 74:
+        alert("you hit j key");
+        break;
+        case 75:
+        alert("you hit k key");
+        break;
+        case 76:
+        alert("you hit l key");
+        break;
+    }
+    
+
+}
+
+        
+document.addEventListener("keydown", keyDownSound);
         
         
         
